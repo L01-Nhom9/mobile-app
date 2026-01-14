2de0409ad73bf031927dda5fc013ae05b53f38ae
yangninhh <241795133+yangninhh@users.noreply.github.com>
2026-01-14 21:40:19 +0700

    deploy

---
+++ backend/src/main/java/com/classtrack/backend/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.classtrack.backend.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+    info = @Info(title = "Jira Clone API", version = "v1"),
+    servers = {
+        @Server(url = "/", description = "Default Server URL"),
+        @Server(url = "/api/java", description = "Gateway Default Server URL")
+    }
+)
+@SecurityScheme(
+    name = "bearerAuth",            // Tên định danh (dùng để gọi trong Controller)
+    type = SecuritySchemeType.HTTP,
+    bearerFormat = "JWT",
+    scheme = "bearer"               // Quan trọng: Báo cho Swagger biết cần prefix "Bearer"
+)
+public class OpenApiConfig {
+    // Không cần code gì bên trong
+}
